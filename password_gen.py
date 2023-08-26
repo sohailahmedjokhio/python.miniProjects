f9e003d4bfb12c6923db74b6c9ab68c1106001f8
Ed Hodapp <ed@hodapp.com>
2023-08-26 15:10:36 -0700

    Start work on no punctuation option.

--- password_gen.py
+++ password_gen.py
@@ -6,7 +6,8 @@ import argparse
 import random
 import string
 
-def get_password(length):
+
+def get_password(length, use_punctuation=True):
     material = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(material) for _ in range(length))
     return password
@@ -16,10 +17,11 @@ def main():
     parser = argparse.ArgumentParser('Random Password Generator')
     parser.add_argument('--length', type=int, default=8, help='random password default value (8)')
     args = parser.parse_args()
-
     password = get_password(args.length)
-
     print('Generated Password Will be:', password)
+    parser = argparse.ArgumentParser('Make Punctuation Useable')
+    parser.add_argument('--punctuation', action='store_true')
+
 
 if __name__ == '__main__':
     main()
--- test_password_gen.py
+++ test_password_gen.py
@@ -1,6 +1,14 @@
+import string
+
 from password_gen import get_password
 
-
 def test_password():
-    passwd = get_password()
-    assert len(passwd) == 8
+    passwd = get_password(10)
+    assert len(passwd) == 10
+
+
+def test_no_punctuation():
+    passwd = get_password(10, False)
+    for c in passwd:
+        assert c not in string.punctuation
+
