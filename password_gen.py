4cd78da1f9663d2112a8e45b8964efc446c4529d
Ed Hodapp <ed@hodapp.com>
2023-08-27 12:08:39 -0700

    Guarantee minimum counts of each char class.

--- password_gen.py
+++ password_gen.py
@@ -8,12 +8,22 @@ import random
 import string
 
 
+def get_one_each_password(length, num_ascii_letters, num_digits, num_punctuation):
+    material = string.ascii_letters + string.digits + string.punctuation
+    one_letter = random.choice(string.ascii_letters)
+    one_digit = random.choice(string.digits)
+    one_punctuation = random.choice(string.punctuation)
+    password = ''.join(random.choices(material, k=length-3)) + one_letter + one_digit + one_punctuation
+    return ''.join(random.sample(password, k=len(password)))
+
+
 def get_password(length, use_punctuation=True):
     material = string.ascii_letters + string.digits
     if use_punctuation is True:
         material += string.punctuation
     password = ''.join(random.choice(material) for _ in range(length))
     return password
+
 
 def main():
     parse = argparse.ArgumentParser(description='Random Password Generator')
@@ -22,6 +32,7 @@ def main():
     args = parse.parse_args()
     password = get_password(args.length, args.use_punctuation)
     print('Generated password will be: ', password)
+
 
 if __name__ == '__main__':
     main()
--- test_password_gen.py
+++ test_password_gen.py
@@ -1,6 +1,6 @@
 import string
 
-from password_gen import get_password
+from password_gen import get_password, get_one_each_password
 
 def test_password():
     passwd = get_password(10)
@@ -12,3 +12,19 @@ def test_no_punctuation():
     for c in passwd:
         assert c not in string.punctuation
 
+
+def test_one_each():
+    ascii_letter_count = 0
+    digits_count = 0
+    punctuation_count = 0
+    passwd = get_one_each_password(10)
+    for c in passwd:
+        if c in string.ascii_letters:
+            ascii_letter_count += 1
+        elif c in string.digits:
+            digits_count += 1
+        elif c in string.punctuation:
+            punctuation_count += 1
+    assert ascii_letter_count >= 1
+    assert digits_count >= 1
+    assert punctuation_count >= 1
