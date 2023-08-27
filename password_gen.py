e9ba2407d4605d31ddc13e7a54dd2940e83c0452
Sohail Ahmed <jansohail818@gmail.com>
2023-08-27 01:43:07 -0700

    Merged changes from remote

--- password_gen.py
+++ password_gen.py
@@ -1,4 +1,5 @@
-"""Random password generator
+"""
+Random password generator
 
 Generates a random password from the command line.
 """
@@ -6,21 +7,21 @@ import argparse
 import random
 import string
 
-def get_password(length):
-    material = string.ascii_letters + string.digits + string.punctuation
+
+def get_password(length, use_punctuation=True):
+    material = string.ascii_letters + string.digits
+    if use_punctuation is True:
+        material += string.punctuation
     password = ''.join(random.choice(material) for _ in range(length))
     return password
 
-
 def main():
-    parser = argparse.ArgumentParser('Random Password Generator')
-    parser.add_argument('--length', type=int, default=8, help='random password default value (8)')
-    args = parser.parse_args()
-
-    password = get_password(args.length)
-
-    print('Generated Password Will be:', password)
+    parse = argparse.ArgumentParser(description='Random Password Generator')
+    parse.add_argument('--length', type=int, default=8, help='Default value of length (8)')
+    parse.add_argument('--use_punctuation', action='store_true', help='Using punctuation')
+    args = parse.parse_args()
+    password = get_password(args.length, args.use_punctuation)
+    print(password)
 
 if __name__ == '__main__':
     main()
-
